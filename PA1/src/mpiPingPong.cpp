759a4957b80cee5de5df74a0eb45bfb32215c6e1
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-02-18 13:37:24 -0800

    Made a static debugging statement

--- PA1/src/mpiPingPong.cpp
+++ PA1/src/mpiPingPong.cpp
@@ -15,6 +15,8 @@ MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
 MPI_Get_processor_name(hostname, &len);
 
 
+printf("The number of tasks is %d\n", numtasks );
+
 
   int ping_pong_count = 0;
   int partner_rank = (taskid + 1) % 2;
