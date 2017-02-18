38446966ee0a09b67c0c33ab51f5a8199a31d9d4
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-02-17 23:00:29 -0800

    This is a BS commit

--- PA1/buildscript.sh
+++ PA1/buildscript.sh
@@ -14,6 +14,11 @@ make
 # running
 make run
 # be sure to add more commands from your makefile here
+
+
+
+
+
 # for example, you may create a target called run_single and run_double
 # (for running on a double box) that contains some special flags.
 # so you may include echo that says what you're doing, and then do it
---
+++ PA1/src/mpiPingPong.cpp
@@ -0,0 +1,42 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define  MASTER		0
+
+int main (int argc, char *argv[])
+{
+int   numtasks, taskid, len;
+char hostname[MPI_MAX_PROCESSOR_NAME];
+
+MPI_Init(&argc, &argv);
+MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
+MPI_Get_processor_name(hostname, &len);
+
+
+
+  int ping_pong_count = 0;
+  int partner_rank = (taskid + 1) % 2;
+  while (ping_pong_count < PING_PONG_LIMIT) 
+  {
+    if (taskid == ping_pong_count % 2) 
+    {
+      // Increment the ping pong count before you send it
+      ping_pong_count++;
+      MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
+      printf("%d sent and incremented ping_pong_count %d to %d\n",
+             taskid, ping_pong_count, partner_rank);
+    } 
+    else 
+    {
+      MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+      printf("%d received ping_pong_count %d from %d\n",
+             taskid, ping_pong_count, partner_rank);
+    }
+  }
+
+MPI_Finalize();
+
+}
+
