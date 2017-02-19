9ea186c6bf1b3b5cbf6613cb45960ca509c412cb
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-02-18 16:40:44 -0800

    Added a timer

--- PA1/src/mpiPingPong.cpp
+++ PA1/src/mpiPingPong.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define  MASTER		0
-#define  PING_PONG_LIMIT 10
+#define  PING_PONG_LIMIT 100000
 
 int main (int argc, char *argv[])
 {
@@ -29,15 +29,15 @@ printf("The number of tasks is %d\n", numtasks );
       // Increment the ping pong count before you send it
       ping_pong_count++;
       MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
-      printf("Proccesor %d sent and incremented ping_pong_count %d to processor %d\n",
-             taskid, ping_pong_count, partner_rank);
+      //printf("Proccesor %d sent and incremented ping_pong_count %d to processor %d\n",
+        //     taskid, ping_pong_count, partner_rank);
     } 
     else 
     {
       MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
                MPI_STATUS_IGNORE);
-      printf("processor %d received ping_pong_count %d from processor %d\n",
-             taskid, ping_pong_count, partner_rank);
+      // printf("processor %d received ping_pong_count %d from processor %d\n",
+         //    taskid, ping_pong_count, partner_rank);
     }
   }
 
