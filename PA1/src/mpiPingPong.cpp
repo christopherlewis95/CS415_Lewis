b9039c8879c580e2623b38e76328aa78c83252ec
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-02-20 18:41:57 -0800

    Newer Approach

--- PA1/src/mpiPingPong.cpp
+++ PA1/src/mpiPingPong.cpp
@@ -14,6 +14,7 @@ int PING_PONG_LIMIT = 10;
 //ofstream fout;
 
 FILE *fp;
+FILE *fp2;
 
 
 MPI_Init(&argc, &argv);
@@ -24,14 +25,18 @@ MPI_Get_processor_name(hostname, &len);
 
 //printf("The number of tasks is %d\n", numtasks );
 
-fp=fopen("times.txt", "w");
+if( taskid == 0 )
+fp=fopen("timesOneBox.txt", "w");
+
+else if( taskid == 1 )
+fp2=fopen("timesTwoBox.txt", "w");
 
   int ping_pong_count;
   int partner_rank = (taskid + 1) % 2;
 
 while( PING_PONG_LIMIT <= 10000)
 {
-	ping_pong_count = 0;
+ ping_pong_count = 0;
   start = MPI_Wtime();
   while (ping_pong_count < PING_PONG_LIMIT) 
   {
@@ -60,9 +65,13 @@ while( PING_PONG_LIMIT <= 10000)
   finish = MPI_Wtime();
   writeout = finish - start;
 
+  if( taskid == 0 )
   fprintf(fp, "%f\t%d\n", writeout, PING_PONG_LIMIT);
  // fout >> writeout >> "   " >> PING_PONG_LIMIT >> endl;
 
+
+else if( taskid == 1 )
+  fprintf(fp2, "%f\t%d\n", writeout, PING_PONG_LIMIT);
 
 
  PING_PONG_LIMIT++;
