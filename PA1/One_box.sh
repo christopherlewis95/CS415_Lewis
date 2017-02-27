32a65247100c2907451c7463f1a9faf78881b891
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-02-26 21:18:00 -0800

    TSST

--- PA1/One_box.sh
+++ PA1/One_box.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-#SBATCH -n 2
-#SBATCH --mem=2048MB
-#SBATCH --time=00:30:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
 #SBATCH --mail-type=ALL
-
-srun src/mpiPingPong.cpp fileOne.txt
+make runOne
 
--- PA1/src/mpiPingPong.cpp
+++ PA1/src/mpiPingPong.cpp
@@ -10,7 +10,8 @@ int main (int argc, char *argv[])
 int   numtasks, taskid, len;
 char hostname[MPI_MAX_PROCESSOR_NAME];
 double start, finish, writeout;
-int PING_PONG_LIMIT = 10;
+int PING_PONG_LIMIT = 1;
+int MAX_LIMIT = atoi(argv[2]);
 //ofstream fout;
 
 FILE *fp;
@@ -34,7 +35,7 @@ fp=fopen(argv[1], "w");
   int ping_pong_count;
   int partner_rank = (taskid + 1) % 2;
 
-while( PING_PONG_LIMIT <= 10000)
+while( PING_PONG_LIMIT <= MAX_LIMIT)
 {
  ping_pong_count = 0;
   start = MPI_Wtime();
@@ -44,7 +45,7 @@ while( PING_PONG_LIMIT <= 10000)
     if (taskid == 0) 
     {
       // Increment the ping pong count before you send it
-      ping_pong_count++;
+                  ping_pong_count++;
       MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
             MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
                MPI_STATUS_IGNORE);
@@ -53,7 +54,7 @@ while( PING_PONG_LIMIT <= 10000)
     } 
     else if (taskid == 1)
     {
-      ping_pong_count++;
+                  ping_pong_count++;
       MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
       MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
                MPI_STATUS_IGNORE);
