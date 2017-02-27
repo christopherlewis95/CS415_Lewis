a014a6df11be9734e90aeacfa8399cdb7df9719d
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-02-26 21:47:17 -0800

    THIS IS SO STUPID

--- PA1/buildscript.sh
+++ PA1/buildscript.sh
@@ -7,24 +7,8 @@ if [ ! -d "$BUILD_DIR" ]; then
 fi
 cd build
 cp ../makefile .
+cp ../One_box.sh
 
 # building
 make
 
-# running
-echo "Running on single Box"
-make runOne
-echo "Running on Different Boxes"
-make runTwo
-# be sure to add more commands from your makefile here
-# for example, you may create a target called run_single and run_double
-# (for running on a double box) that contains some special flags.
-# so you may include echo that says what you're doing, and then do it
-# Ex:)
-# echo "PA1 on single box" && make run_single
-
-
-# cleaning (optional for build script)
-make clean
-
-cd ..
