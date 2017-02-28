5d62b3e5cbb4e490c0674403f927b36790a6f987
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-02-27 20:45:41 -0800

    UGH

--- PA1/One_box.sh
+++ PA1/One_box.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-echo "runOne"
+#SBATCH -n 2
+#SBATCH -t 0-1:30
+#SBATCH -J One_box
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
-#SBATCH --mail-type=ALL
-make runOne
+#SBATCH --mail-type=END
+
+
+echo "runOne"
+srun ./One_box
 
