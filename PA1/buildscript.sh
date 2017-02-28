a1b4259a8cd81b9b6a1707dba3a9296c151f6e5a
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-02-27 23:20:36 -0800

    PLEASE

--- PA1/buildscript.sh
+++ PA1/buildscript.sh
@@ -6,9 +6,29 @@ if [ ! -d "$BUILD_DIR" ]; then
 	mkdir build
 fi
 cd build
-cp ../makefile
-cp ../One_box.sh
+cp ../makefile .
 
 # building
 make
 
+# running
+echo "Running on single Box"
+make runOne
+echo "Running on Different Boxes"
+make runTwo
+
+echo "Running on Different Boxes/Timed"
+make timed
+
+# be sure to add more commands from your makefile here
+# for example, you may create a target called run_single and run_double
+# (for running on a double box) that contains some special flags.
+# so you may include echo that says what you're doing, and then do it
+# Ex:)
+# echo "PA1 on single box" && make run_single
+
+
+# cleaning (optional for build script)
+make clean
+
+cd ..
