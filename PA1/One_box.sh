0878c42381792cee417722a65b34d66bc4c82249
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-02-27 22:28:29 -0800

    STUPID

--- PA1/One_box.sh
+++ PA1/One_box.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-#SBATCH -n 2
+#SBATCH -n2
 #SBATCH --time=00:01:00
-#SBATCH -J One_box
-#SBATCH --mail-user=christopherlewis@nevada.unr.edu
-#SBATCH --mail-type=END
 
-echo "runOne"
-srun One_box oneBoxPing.txt 10
+srun One_box
 
