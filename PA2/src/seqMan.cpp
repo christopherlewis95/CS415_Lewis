ad56cf988464700d5f97463303aa877406531c14
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-03-11 16:52:58 -0800

    SEQ MAN

--- PA2/src/seqMan.cpp
+++ PA2/src/seqMan.cpp
@@ -22,7 +22,7 @@ int main()
     complex c;
     
     bool good;
-   const char * const fileName;
+   const char * const fileName = "image.ppm";
 
     int display_width = 680;
     int display_height = 480;
@@ -31,7 +31,7 @@ int main()
 
     for( int i = 0; i < display_height; i++ )
     {
-        pix = new unsigned char [display_width];
+        pix[i] = new unsigned char[display_width];
 
 
     }
@@ -40,7 +40,7 @@ int main()
     real_min = -2;
     real_max = 2;
     
-    imag_min = 1;
+    imag_min = -1;
     imag_max = imag_min + (real_max-real_min)*display_height/display_width;
     
     // Set scale;
@@ -58,15 +58,15 @@ int main()
             c.real = real_min + x * (real_max - real_min)/display_width;
             c.imag = imag_min + y * (imag_max - imag_min)/display_height;
             static int n = cal_pixel(c);
-
-            pim_write_black_and_white(fileName, display_width, display_height,(const unsigned char **)pix);
+            pix[y][x] = n;
+           
 
 
             }
 
 
     }
-
+ pim_write_black_and_white(fileName, display_width, display_height,(const unsigned char **)pix);
 
 
 
@@ -77,22 +77,24 @@ int main()
 //// FUNCTIONS
 
 int cal_pixel(complex c)
-    {
+{
+    int count, maxIter;
+    complex z;
+    float temp, lengthSq;
 
-        int count, max_iter;
-        complex z;
-        float temp, lengthsq;
-        max_iter = 256;
-        z.real = 0;
-        z.imag = 0;
-        count = 0;
-        do{
-            temp = z.real = 2;
-            z.imag = 2 * z.real - z.imag * z.imag + c.real;
-            z.real = temp;
-            lengthsq = z.real * z.real + z.imag * z.imag;
-            count++;
-        } while((lengthsq < 4.0) && (count < max_iter));
+
+    maxIter = 256;
+    z.real = 0;
+    z.imag = 0;
+    count = 0;
+    do{
+        temp = z.real * z.real - z.imag * z.imag + c.real;
+        z.imag = 2 * z.real * z.imag + c.imag;
+        z.real = temp;
+        lengthSq = z.real * z.real+z.imag*z.imag;
+        count++;
+    }while( (lengthSq < 4.0) && (count < maxIter) );
+
     return count;
-    }
+}
 
