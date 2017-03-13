7a4aaaec6513574121bd35173924abc7edc15c20
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-03-12 20:13:46 -0700

    SAGG

--- PA2/seqMan.sh
+++ PA2/seqMan.sh
@@ -3,7 +3,7 @@
 #SBATCH -n1
 #SBATCH --time=00:01:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
-for (( a=600; c<=3000; c+=100SS ))
+for (( a=600; a<=3000; a+=100 ))
 do  
    echo $a
    srun seqMan $a $a
