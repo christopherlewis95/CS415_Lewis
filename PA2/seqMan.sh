7bebd85b0860f83828a9a5fe45945ade62aa0098
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-03-12 19:48:39 -0700

    changed script

--- PA2/seqMan.sh
+++ PA2/seqMan.sh
@@ -3,11 +3,10 @@
 #SBATCH -n1
 #SBATCH --time=00:01:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
-until [ $a -lt 3000 ]
+a=0
+while [ $a -lt 3000 ]
 do
    echo $a
    srun seqMan $a $a
    a=expr $a + 100`
 done
-   
-
