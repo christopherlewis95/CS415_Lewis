1fe8c56868bca6049a2e62687f48cf9f2c496c9c
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-03-13 21:40:21 -0700

    NUFFER

--- PA2/dynAssMan.sh
+++ PA2/dynAssMan.sh
@@ -3,7 +3,7 @@
 #SBATCH -n5
 #SBATCH --time=00:05:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
-for (( a=600; a<=2500; a+=100 ))
+for (( a=5000; a<=6000; a+=100 ))
 do  
    echo $a
    srun dynMan $a $a
