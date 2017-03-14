9c5e4ccb2b2c195be9ac47f1e177620fedcf4110
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-03-13 22:05:07 -0700

    DONE AND DONE

--- PA2/dynAssMan.sh
+++ PA2/dynAssMan.sh
@@ -3,7 +3,7 @@
 #SBATCH -n5
 #SBATCH --time=00:05:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
-for (( a=5000; a<=6000; a+=100 ))
+for (( a=400; a<=1800; a+=20 ))
 do  
    echo $a
    srun dynMan $a $a
