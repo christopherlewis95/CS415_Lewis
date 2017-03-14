c7c58bf775771c5749c9b4c353f5654abfa4cdf5
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-03-14 11:48:46 -0700

    Changed a IO Err

--- PA2/src/dynAssMan.cpp
+++ PA2/src/dynAssMan.cpp
@@ -55,7 +55,7 @@ int main( int argc, char **argv ) {
 	MPI_Init( &argc, &argv );
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 	MPI_Comm_size( MPI_COMM_WORLD, &numProcessors );
-    cout << display_height << " and " << display_width " WITH " << num_processors << endl;
+    cout << display_height << " and " << display_width << " WITH " << num_processors << endl;
 
 	if( rank == 0 ) // Master
     { 
