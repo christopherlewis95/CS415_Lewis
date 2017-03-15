c67e6322cc7ae4df6168b923bb7c9c0390c4d3d0
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-03-14 18:36:15 -0700

    Changed time limit for more data analysis

--- PA2/dynAssMan.sh
+++ PA2/dynAssMan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N3
 #SBATCH -n12
-#SBATCH --time=00:05:00
+#SBATCH --time=00:10:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
 for (( a=400; a<=10000; a+=100 ))
 do  
