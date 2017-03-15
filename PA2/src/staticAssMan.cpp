5efb1c407b6065f380e3682df76572e4b1e52d85
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-03-14 17:33:48 -0700

    Changed static for dynamic allocation

--- PA2/src/staticAssMan.cpp
+++ PA2/src/staticAssMan.cpp
@@ -5,7 +5,6 @@
 * AUTHOR: Blaise Barney
 * LAST REVISED: 03/05/10
 ******************************************************************************/
-/// THIS IS A USELESS COMMENT
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
