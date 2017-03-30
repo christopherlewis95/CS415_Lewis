cd5cbbf384d1d2ab17c88cf27d593a3c887147dc
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-03-29 19:26:05 -0700

    Changed the files

--- PA3/seqBucket.sh
+++ PA3/seqBucket.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 #SBATCH -N1
-#SBATCH -n8
+#SBATCH -n1
 #SBATCH --time=01:00:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
-for (( a=400; a<=30000; a+=100 ))
-do  
-   echo $a
-   srun dynMan $a $a
-done
+srun seqBucket
+
