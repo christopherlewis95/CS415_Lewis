db1f8646defa78d46c980c35809399ce561b93b3
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-04-02 16:14:01 -0700

    Changed line 29

--- PA3/src/parBucketSort.cpp
+++ PA3/src/parBucketSort.cpp
@@ -26,7 +26,7 @@ int main( int argc, char **argv ) {
     MPI_Init( &argc, &argv );
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 	MPI_Comm_size( MPI_COMM_WORLD, &numProcessors );
-    int numProcessors;
+    //int numProcessors;
     int *arr;
     int i, j; 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
