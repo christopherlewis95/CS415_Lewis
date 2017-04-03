0f455a1d52cccb5d9b0bce5aeb43ab9a1b437d60
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-04-02 22:05:32 -0700

    Chnaged Data tag

--- PA3/src/parBucketSort.cpp
+++ PA3/src/parBucketSort.cpp
@@ -12,6 +12,7 @@ using namespace std;
 #define TERMINATE_TAG 0
 #define DATA_TAG 1
 #define RESULT_TAG 2
+#define MY_MPI_DATA_TAG 3
 
 // complex struct
 
@@ -62,7 +63,7 @@ int main( int argc, char **argv ) {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	else { 
         MPI_Status status;	
-        MPI_Recv( &arr, size/numProcessors, MPI_INT, 0, MPI_DATA_TAG, MPI_COMM_WORLD, &status ); // '0' needs to be master variable
+        MPI_Recv( &arr, size/numProcessors, MPI_INT, 0, MY_MPI_DATA_TAG, MPI_COMM_WORLD, &status ); // '0' needs to be master variable
         cout << arr[0] << endl;
 
     }
