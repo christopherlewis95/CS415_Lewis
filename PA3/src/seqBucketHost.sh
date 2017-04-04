26f44418f81f28950facef7114cbda11abbb695a
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-04-03 17:44:17 -0700

    changed to rediculous upper limit

--- PA3/src/seqBucketHost.sh
+++ PA3/src/seqBucketHost.sh
@@ -3,9 +3,9 @@
 #SBATCH -n1
 #SBATCH --time=00:05:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
-for (( a=100000; a<=10000000; a+=100000 ))
+for (( a=100000; a<=100000000; a+=100000 ))
 do  
-    srun seqBucket $a seqBucketSort100000.txt   
+    srun seqBucket $a seqBucketSortMax100000000.txt   
 done
 
 
