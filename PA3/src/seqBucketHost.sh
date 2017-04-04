6996bd38d0f538acc0b76aa5865fd75b05bc6431
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-04-03 17:31:32 -0700

    chengdd the script

--- PA3/src/seqBucketHost.sh
+++ PA3/src/seqBucketHost.sh
@@ -3,9 +3,9 @@
 #SBATCH -n1
 #SBATCH --time=00:05:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
-for (( a=10000; a<=10000000; a+=10000 ))
+for (( a=100000; a<=10000000; a+=100000 ))
 do  
-    srun seqBucket $a seqBucketSort.txt   
+    srun seqBucket $a seqBucketSort100000.txt   
 done
 
 
