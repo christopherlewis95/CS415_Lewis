9f37e9b9a36660d661de4c4178e2931c31dbdc4e
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-04-04 23:21:25 -0700

    Added time

--- PA3/src/seqBucketHost.sh
+++ PA3/src/seqBucketHost.sh
@@ -3,7 +3,7 @@
 #SBATCH -n1
 #SBATCH --time=00:05:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
-for (( a=100; a<=1000; a+=100 ))
+for (( a=100000; a<=10000000000; a+=100000 ))
 do  
     srun seqBucket $a seqBucketSortMax100.txt   
 done
