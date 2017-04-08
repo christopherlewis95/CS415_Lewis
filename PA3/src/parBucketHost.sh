816671b6cd6f37b26d454470e795d6db33e66477
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-04-08 13:35:06 -0700

    ParBucket

--- PA3/src/parBucketHost.sh
+++ PA3/src/parBucketHost.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -N1
-#SBATCH -n8
+#SBATCH -n2
 #SBATCH --time=00:05:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
-for (( a=100; a<=1000; a+=100 ))
+for (( a=10; a<=50; a+=10 ))
 do  
     srun parBucket $a
 done
