fa9fd73b34e3e114ae20bbcfd312ea98ec49211b
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-04-09 18:07:16 -0700

    changed stuff

--- PA3/src/parBucketHost4Cores.sh
+++ PA3/src/parBucketHost4Cores.sh
@@ -3,7 +3,7 @@
 #SBATCH -n2
 #SBATCH --time=00:30:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
-for (( a=1000000; a<=15000000; a+=1000000 ))
+for (( a=10000; a<=15000000; a+=10000 ))
 do  
     srun parBucket $a 2Cores.txt
 done
