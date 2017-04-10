b9d4fc381ae75e6aee4f37b01d26954bab04a13f
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-04-09 18:01:20 -0700

    changed to two

--- PA3/src/parBucketHost4Cores.sh
+++ PA3/src/parBucketHost4Cores.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -N1
-#SBATCH -n4
+#SBATCH -n2
 #SBATCH --time=00:30:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
 for (( a=100; a<=1500; a+=100 ))
 do  
-    srun parBucket $a fourCores.txt
+    srun parBucket $a 2Cores.txt
 done
 
