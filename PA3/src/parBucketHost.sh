a7fb7826b118ed8efbeaa172282544cf07495453
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-04-09 17:25:36 -0700

    coresChanged

--- PA3/src/parBucketHost.sh
+++ PA3/src/parBucketHost.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH -N2
-#SBATCH -n10
-#SBATCH --time=00:05:00
+#SBATCH -N1
+#SBATCH -n4
+#SBATCH --time=00:20:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
-file=10Cores.txt
-for (( a=10; a<=100; a+=10 ))
+for (( a=100; a<=1500; a+=100 ))
 do  
-    srun parBucket $a $file
+    srun parBucket $a 
 done
 
