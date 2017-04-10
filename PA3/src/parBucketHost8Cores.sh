8e75c875fb078f1ac7b6464cad9f3af7831371d3
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-04-09 19:05:46 -0700

    fixed stuff

--- PA3/src/parBucketHost8Cores.sh
+++ PA3/src/parBucketHost8Cores.sh
@@ -3,7 +3,7 @@
 #SBATCH -n8
 #SBATCH --time=01:00:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
-for (( a=10000000; a<=1500000000; a+=10000000 ))
+for (( a=100000000; a<=1500000000; a+=100000000 ))
 do  
     srun parBucket $a eightCores.txt
 done
