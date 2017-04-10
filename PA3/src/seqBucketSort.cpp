56a96cb4c83264f3a1622d7732df95c46157b0a8
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-04-09 20:39:07 -0700

    changed stuff

--- PA3/src/seqBucketSort.cpp
+++ PA3/src/seqBucketSort.cpp
@@ -60,7 +60,7 @@ int main(int argc, char** argv)
   int n = sizeof (input_ar) / sizeof (input_ar[0]);
 
   //cout << "ENTERING" << endl;
-  bucketSort(input_ar, size, 0, BIGGEST, argv);
+  bucketSort(input_ar, amount, 0, BIGGEST, argv);
 
   //clockTicks = clock() - t0;
  //cout << "EXITING" << endl;
