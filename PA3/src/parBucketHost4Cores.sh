6db150eff70f5e6a43ba3f7007970e0e03b1c046
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-04-09 19:04:22 -0700

    changed scripts

--- PA3/src/parBucketHost4Cores.sh
+++ PA3/src/parBucketHost4Cores.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N1
 #SBATCH -n4
-#SBATCH --time=00:30:00
+#SBATCH --time=01:00:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
 for (( a=10000000; a<=1500000000; a+=10000000 ))
 do  
