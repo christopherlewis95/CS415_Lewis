172e3f4b5e9cabcc59a8ba0fa64b732f90c82cfc
Christopher Lewis <christopherlewis@nevada.unr.edu>
2017-04-12 18:18:22 -0700

    Got rid of old file

--- PA4/src/seqBucketHost.sh
+++ PA4/src/seqBucketHost.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
 for (( a=100000; a<=10000000000; a+=100000 ))
 do  
-    srun seqBucket $a seqBucketSortMax100.txt   
+       
 done
 
 
--- PA4/src/seqMatrix.cpp
+++ PA4/src/seqMatrix.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <time.h>
 
+
 using namespace std;
 
 void genNumbers( int **arrayA, int **arrayB, int size );
@@ -12,7 +13,10 @@ void genNumbers( int **arrayA, int **arrayB, int size );
 
 int main(int argc, char** argv)
     {
+    clock_t startProgram, endProgram;
+    FILE *fp;
         int sizeN = atoi(argv[1]);
+        fp = fopen(argv[2], "a+");
         int **arrayA = new int *[sizeN];
         int **arrayB = new int *[sizeN];
         int **arrayC = new int *[sizeN];  
@@ -30,30 +34,11 @@ int main(int argc, char** argv)
 
     // x denotes number rows in matrix A
     // y denotes number columns in matrix A
-    cout << "\n\nMatrix A :\n\n";
-    for (i = 0; i < sizeN; i++)
-    {
-        for (j = 0; j < sizeN; j++)
-        {
-            cout << "\t" << arrayA[i][j];
-        }
-        cout << "\n\n";
-    }
-    cout << "\n-----------------------------------------------------------\n";
     // m denotes number rows in matrix B
     // n denotes number columns in matrix B
  
-    cout << "\n\nMatrix B :\n\n";
-    for (i = 0; i < sizeN; i++)
-    {
-        for (j = 0; j < sizeN; j++)
-        {
-            cout << "\t" << arrayB[i][j];
-        }
-        cout << "\n\n";
-    }
 
-
+        startProgram = clock();
         for (i = 0; i < sizeN; i++)
         {
             for (j = 0; j < sizeN; j++)
@@ -65,17 +50,8 @@ int main(int argc, char** argv)
                 }
             }
         }
-        cout
-                << "\n-----------------------------------------------------------\n";
-        cout << "\n\nMultiplication of Matrix A and Matrix B :\n\n";
-        for (i = 0; i < sizeN; i++)
-        {
-            for (j = 0; j < sizeN; j++)
-            {
-                cout << "\t" << arrayC[i][j];
-            }
-            cout << "\n\n";
-        }
+        endProgram = clock();
+        fprintf(fp, "%d \t %f", sizeN, ((float)(endProgram-startProgram)/CLOCKS_PER_SEC));
         delete[] arrayA;
         delete[] arrayB;
         delete[] arrayC;
@@ -106,4 +82,3 @@ void genNumbers( int **arrayA, int **arrayB, int size )
                 }
         }
 
-    }
