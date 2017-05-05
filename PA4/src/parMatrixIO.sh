9871cf3054381e32cce5cb1e9799e776ac30c74a
Christopher J Lewis <christopherlewis@nevada.unr.edu>
2017-05-05 12:14:30 -0700

    This is :D

--- PA4/src/parMatrixIO.sh
+++ PA4/src/parMatrixIO.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -N1
-#SBATCH -n4
+#SBATCH -N2
+#SBATCH -n9
 #SBATCH --time=00:01:00
 #SBATCH --mail-user=christopherlewis@nevada.unr.edu
 srun parMatrix 12 matrixA.txt matrixB.txt 
